1f17e75e5d3a27f1851c792b722fe60150376a2f
MoniqueSC <mscbg@hotmail.com>
2024-02-14 19:39:16 -0300

    Tarefa cypress

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,18 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach (() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Abominable Hoodie')
+        .click()
+        cy.get('.product_title').should('contain','Abominable Hoodie')
+    })
+})
