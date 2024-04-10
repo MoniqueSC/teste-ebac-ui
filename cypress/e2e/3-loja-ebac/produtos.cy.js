7e3c073b0cfc30b710f3a9b1686b91ca22f38706
MoniqueSC <mscbg@hotmail.com>
2024-04-10 18:43:44 -0300

    Aula 1004

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,8 +1,9 @@
 ///<reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
     beforeEach (() => {
-        cy.visit('produtos')
+       produtosPage.visitarUrl()
 
 
     });
@@ -12,12 +13,46 @@ describe('Funcionalidade: Produtos', () => {
         })
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Abominable Hoodie')
-        .click()
-        cy.get('.product_title').should('contain','Abominable Hoodie')
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain','Descrição')
+    })
+    it('Deve selecionar um produto da lista',() => {
+
+    });
+
+    it('Deve buscar um produto com sucesso',() => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    })
+    it('Deve visitar a página do produto', () =>{
+        produtosPage.visitarProduto('Aero-Daily-Fitness-Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee')
+    })
+    it('Deve adicionar produto ao carrinho',()=>{
+        let qtd = 5
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+        produtosPage.addProdutoCarrinho('M', 'Yellow', qtd)
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+       
+        
+    })
+    it('Deve adicionar produto ao carrinho buscando da massa de dados',()=>{
+        
+        cy.fixture('produtos').then(dados => {
+            
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor,
+                 dados[0].quantidade)
+                 
+            
+                 cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+        
+        
+       
+        
     })
 })
